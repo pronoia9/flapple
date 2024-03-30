8ca4e0b5205e5612e2179010f620101aee358f33
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-30 21:22:07 +0300

    add title + gsap

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,18 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
 export const Hero = () => {
-  return <div>Hero</div>;
+  useGSAP(() => {
+    gsap.to('#hero', { opacity: 1, delay: 2 });
+  }, []);
+
+  return (
+    <section className='w-full nav-height bg-black relative'>
+      <div className='h-5/6 w-full flex-center flex-col'>
+        <p id='hero' className='hero-title'>
+          iPhone 15 Pro
+        </p>
+      </div>
+    </section>
+  );
 };
