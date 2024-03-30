21e039af29236471c98f48b9bef4e46f581cff9d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-30 19:32:07 +0300

    update styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,13 @@ import { appleImg, bagImg, searchImg } from '../utils';
 
 export const Navbar = () => {
   return (
-    <header className='w-full py-5 sm:px-10 px-5'>
-      <nav className='w-full screen-max-width flex justify-between items-center'>
-        <img src={appleImg} alt='logo' width={14} height={18} />
+    <header className='w-full screen-max-width m-auto flex py-5 px-5'>
+      <nav className='w-full flex justify-between items-center gap-8'>
+        <div className='max-md:grow'>
+          <img src={appleImg} alt='logo' width={14} height={18} />
+        </div>
 
-        <div className='flex gap-10 flex-grow-1 justify-center max-sm:hidden'>
+        <div className='grow flex justify-between items-center max-md:hidden'>
           {navLists.map((nav, index) => (
             <a key={`nav-${index}`} href={nav.link} className='text-sm text-gray hover:text-white transition-all'>
               {nav.title}
@@ -15,14 +17,12 @@ export const Navbar = () => {
           ))}
         </div>
 
-        <div className='flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1'>
-          <a href='https://www.apple.com/us/search'>
-            <img src={searchImg} alt='search' width={18} height={18} />
-          </a>
-          <a href='https://www.apple.com/us/shop/goto/bag'>
-            <img src={bagImg} alt='bag' width={18} height={18} />
-          </a>
-        </div>
+        <a href='https://www.apple.com/us/search'>
+          <img src={searchImg} alt='search' width={18} height={18} />
+        </a>
+        <a href='https://www.apple.com/us/shop/goto/bag'>
+          <img src={bagImg} alt='bag' width={18} height={18} />
+        </a>
       </nav>
     </header>
   );
