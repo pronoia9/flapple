580a8a55b708737c5901a4c26c5bd0e1ee8aabb8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-30 18:44:40 +0300

    update styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import { appleImg, bagImg, searchImg } from '../utils';
 
 export const Navbar = () => {
   return (
-    <header className='w-full py-5 sm:px-10 px-5 flex justify-between items-center'>
-      <nav className='w-full screen-max-width flex'>
+    <header className='w-full py-5 sm:px-10 px-5'>
+      <nav className='w-full screen-max-width flex justify-between items-center'>
         <img src={appleImg} alt='logo' width={14} height={18} />
 
-        <div className='flex gap-10 flex-1 justify-center max-sm:hidden'>
+        <div className='flex gap-10 flex-grow-1 justify-center max-sm:hidden'>
           {navLists.map((nav, index) => (
             <a key={`nav-${index}`} href={nav.link} className='text-sm text-gray hover:text-white transition-all'>
               {nav.title}
