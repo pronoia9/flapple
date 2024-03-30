c1caf39be1af4f22eaa4d00b9f37754030b49137
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-03-30 18:25:10 +0300

    add init content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,28 @@
+import { appleImg, bagImg, searchImg } from '../utils';
+
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header>
+      <nav>
+        <img src={appleImg} alt='logo' width={14} height={18} />
+
+        <div>
+          {['Mac', 'iPad', 'iPhone', 'Watch'].map((nav, index) => (
+            <a key={`nav-${index}`} href={`https://www.apple.com/${nav.toLowerCase()}/`} className=''>
+              {nav}
+            </a>
+          ))}
+        </div>
+
+        <div>
+          <a href='https://www.apple.com/us/search'>
+            <img src={searchImg} alt='search' width={18} height={18} />
+          </a>
+          <a href='https://www.apple.com/us/shop/goto/bag'>
+            <img src={bagImg} alt='bag' width={18} height={18} />
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
 };
