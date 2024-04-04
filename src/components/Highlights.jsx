b293a0f9677f85e49ee93fabb75debefc25ecd12
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-04 16:20:57 +0300

    add links

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,9 +1,12 @@
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 
+import { rightImg, watchImg } from '../utils';
+
 export const Highlights = () => {
   useGSAP(() => {
     gsap.to('#title', { opacity: 1, y: 0 });
+    gsap.to('.link', { opacity: 1, y: 0, duration: 1, stagger: 0.25 });
   }, []);
 
   return (
@@ -13,6 +16,17 @@ export const Highlights = () => {
           <h1 id='title' className='section-heading'>
             Get the highlights.
           </h1>
+
+          <div className='flex flex-wrap items-end gap-5'>
+            <p className='link'>
+              Watch the film
+              <img src={watchImg} alt='watch' className='ml-2' />
+            </p>
+            <p className='link'>
+              Watch the event
+              <img src={rightImg} alt='right' className='ml-2' />
+            </p>
+          </div>
         </div>
       </div>
     </section>
