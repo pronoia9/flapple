31334eb7f17d3a81a25343bb0d01307dc726b89f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-17 13:17:02 -0400

    add init video + text

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,3 +1,29 @@
+import { hightlightsSlides } from '../data';
+
 export const VideoCarousel = () => {
-  return <div>VideoCarousel</div>;
+  return (
+    <>
+      <div className='flex items-center'>
+        {hightlightsSlides.map((slide, i) => (
+          <div key={slide.id} id='slider' className='sm:pr-20 pr-10'>
+            <div className='video-carousel_container'>
+              <div className='w-full h-full flex-center rounded-3xl overflow-hidden bg-black'>
+                <video id='video' playsInline preload='auto' muted>
+                  <source src={slide.video} type='video/mp4' />
+                </video>
+              </div>
+
+              <div className='absolute top-12 left-[5%] z-10'>
+                {slide.textLists.map((text, i) => (
+                  <p key={i} className='md:text-2xl text-xl font-medium'>
+                    {text}
+                  </p>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
