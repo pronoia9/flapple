859f744993516c3f29da6740be509506ff1b7a57
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-17 16:30:40 -0400

    remove cursor-pointer from progress

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -144,7 +144,7 @@ export const VideoCarousel = () => {
         {/* carousel progress */}
         <div className='flex-center py-5 px-7 bg-gray-300 backdrop-blur rounded-full'>
           {videoRef.current.map((_, i) => (
-            <span key={i} ref={(el) => (videoDivRef.current[i] = el)} className='mx-2 w-3 h-3 bg-gray-200 rounded-full relative cursor-pointer'>
+            <span key={i} ref={(el) => (videoDivRef.current[i] = el)} className='mx-2 w-3 h-3 bg-gray-200 rounded-full relative'>
               <span ref={(el) => (videoSpanRef.current[i] = el)} className='absolute h-full w-full rounded-full' />
             </span>
           ))}
