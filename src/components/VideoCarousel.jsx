cd4302999a7037e0d941aa63cd5bc69f3022bf5a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-17 13:54:22 -0400

    add init carousel progress

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -64,6 +64,17 @@ export const VideoCarousel = () => {
           </div>
         ))}
       </div>
+
+      {/* carousel progress + play/pause button */}
+      <div className='relative flex-center mt-10'>
+        <div className='flex-center py-5 px-7 bg-gray-300 backdrop-blur rounded-full'>
+          {videoRef.current.map((_, i) => (
+            <span key={`${i}`} ref={(el) => (videoDivRef.current[i] = el)} className='mx-2 w-3 h-3 bg-gray-200 rounded-full relative cursor-pointer'>
+              <span ref={(el) => (videoSpanRef.current[i] = el)} className='absolute h-full w-full rounded-full' />
+            </span>
+          ))}
+        </div>
+      </div>
     </>
   );
 };
