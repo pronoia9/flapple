b4aabc9e76931527a4830359d57d648c78459038
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-18 16:16:31 -0400

    add subtitles

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -46,6 +46,23 @@ export const Features = () => {
                   <img className='feature-video g_grow' src={explore2Img} alt='titanium 2' />
                 </div>
               </div>
+
+              <div className='feature-text-container'>
+                <div className='flex-1 flex-center'>
+                  <p className='feature-text g_text'>
+                    iPhone 15 Pro is <span className='text-white'>the first iPhone to feature an aerospace-grade titanium design</span>, using the
+                    same alloy that spacecrafts use for missions to Mars.
+                  </p>
+                </div>
+
+                <div className='flex-1 flex-center'>
+                  <p className='feature-text g_text'>
+                    Titanium has one of the best strength-to-weight ratios of any metal, making these our{' '}
+                    <span className='text-white'>lightest Pro models ever.</span>
+                    You'll notice the difference the moment you pick one up.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
