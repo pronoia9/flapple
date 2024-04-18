325098e0375b468aa077e8b5b08772183659054d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-18 12:27:48 -0400

    add controls

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,6 +1,8 @@
+import { Suspense, useRef } from 'react';
+import * as THREE from 'three';
 import { OrbitControls, PerspectiveCamera, View } from '@react-three/drei';
 
-import { Lights } from '../components';
+import { IPhone, Lights, Loader } from '../components';
 
 export const ModelView = ({ index, groupRef, gsapType, controlRef, setRotation, item, size }) => {
   return (
@@ -11,6 +13,17 @@ export const ModelView = ({ index, groupRef, gsapType, controlRef, setRotation, 
       {/* lights */}
       <ambientLight intensity={0.3} />
       <Lights />
+
+      {/* controls */}
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotation(controlRef.current.getAzimuthalAngle())}
+      />
     </View>
   );
 };
