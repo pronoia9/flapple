13d04f94a050413e20a1aa6422bab9045cdc4bee
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-18 11:23:47 -0400

    add canvas props

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -52,7 +52,11 @@ export const Model = () => {
               />
             ))}
 
-            <Canvas className='w-full h-full'>
+            <Canvas
+              className='w-full h-full'
+              style={{ position: 'fixed', top: 0, bottom: 0, left: 0, right: 0, overflow: 'hidden' }}
+              eventSource={document.getElementById('root')}
+            >
               <View.Port />
             </Canvas>
           </div>
