be2f45209f371e118a871a46417a347d302448e7
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-18 15:40:20 -0400

    add header + init video

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,8 +1,11 @@
+import { useRef } from 'react';
 import { useGSAP } from '@gsap/react';
 
-import { animateWithGsap } from '../utils';
+import { animateWithGsap, exploreVideo } from '../utils';
 
 export const Features = () => {
+  const videoRef = useRef();
+
   useGSAP(() => {
     animateWithGsap('#features_title', { y: 0, opacity: 1 });
   }, []);
@@ -14,6 +17,21 @@ export const Features = () => {
           <h1 id='features_title' className='section-heading'>
             Explore the full story.
           </h1>
+        </div>
+
+        <div className='flex flex-col justify-center items-center overflow-hidden'>
+          <div className='mt-32 mb-24 pl-24'>
+            <h2 className='text-5xl lg:text-7xl font-semibold'>iPhone.</h2>
+            <h2 className='text-5xl lg:text-7xl font-semibold'>Forged in titanium.</h2>
+          </div>
+
+          <div className='flex-center flex-col sm:px-10'>
+            <div className='relative h-[50vh] w-full flex items-center'>
+              <video ref={videoRef} id='exploreVideo' className='w-full h-full object-cover object-center' preload='none' playsInline muted autoPlay>
+                <source src={exploreVideo} type='video/mp4' />
+              </video>
+            </div>
+          </div>
         </div>
       </div>
     </section>
