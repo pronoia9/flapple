9932d7ef0f49dcfa4d0843acc60b5c90b2c5f325
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-18 11:03:54 -0400

    add title

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,3 +1,18 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
 export const Model = () => {
-  return <div>Model</div>;
+  useGSAP(() => {
+    gsap.to('#heading', { y: 0, opacity: 1 });
+  }, []);
+
+  return (
+    <section className='common-padding'>
+      <div className='screen-max-width'>
+        <h1 id='heading' className='section-heading'>
+          Take a closer look.
+        </h1>
+      </div>
+    </section>
+  );
 };
