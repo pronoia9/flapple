fab0bf04a85e84d42fc08fdec1c14999eaf664d2
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-18 16:37:59 -0400

    add animation to chip

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,6 +1,19 @@
-import { chipImg } from '../utils';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
+import { animateWithGsap, chipImg } from '../utils';
 
 export const HowItWorks = () => {
+  useGSAP(() => {
+    gsap.from('#chip', {
+      opacity: 0,
+      scale: 2,
+      duration: 2,
+      ease: 'power2.inOut',
+      scrollTrigger: { trigger: '#chip', start: '20% bottom' },
+    });
+  }, []);
+
   return (
     <section className='common-padding'>
       <div className='screen-max-width'>
@@ -10,8 +23,8 @@ export const HowItWorks = () => {
 
         <div className='flex flex-col items-center'>
           <h2 className='hiw-title'>
-            A17 Pro chip.<br />
-            A monster win for gaming.
+            A17 Pro chip.
+            <br />A monster win for gaming.
           </h2>
 
           <p className='hiw-subtitle'>It's here. The biggest redesign in the history of Apple GPUs.</p>
