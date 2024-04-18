896064ae4f8cbf201b136ba1f7dca3a0ecfa9bc3
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-18 14:55:30 -0400

    add sentry with profiler

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import * as Sentry from '@sentry/react';
 
 import { Experience, Features, Footer, Hero, Highlights, HowItWorks, Navbar } from './components';
 
-// eslint-disable-next-line react-refresh/only-export-components
 export default Sentry.withProfiler(() => (
   <main className='bg-black'>
     <Navbar />
