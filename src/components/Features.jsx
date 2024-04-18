c0d85ae79acb4660433f84acc95c8ba9cf3d6a45
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-18 15:27:19 -0400

    add heading

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,3 +1,21 @@
+import { useGSAP } from '@gsap/react';
+
+import { animateWithGsap } from '../utils';
+
 export const Features = () => {
-  return <div>Features</div>;
+  useGSAP(() => {
+    animateWithGsap('#features_title', { y: 0, opacity: 1 });
+  }, []);
+
+  return (
+    <section className='h-full common-padding bg-zinc relative overflow-hidden'>
+      <div className='screen-max-wdith'>
+        <div className='mb-12 w-full'>
+          <h1 id='features_title' className='section-heading'>
+            Explore the full story.
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
 };
