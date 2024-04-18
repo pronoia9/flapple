e5099e5db1d30f82bb92cd18fcc284ba336f21da
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-18 11:09:25 -0400

    init states

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 
+import { ModelView } from '../components';
+import { yellowImg } from '../utils';
+
 export const Model = () => {
+  const [size, setSize] = useState('small'),
+    [model, setModel] = useState({
+      title: 'iPhone 15 Pro in Natural Titanium',
+      color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
+      img: yellowImg,
+    });
+
   useGSAP(() => {
     gsap.to('#heading', { y: 0, opacity: 1 });
   }, []);
@@ -12,6 +23,12 @@ export const Model = () => {
         <h1 id='heading' className='section-heading'>
           Take a closer look.
         </h1>
+
+        <div className='flex flex-col items-center mt-5'>
+          <div className='w-full h-[75vh] md:h-[90vh] overflow-hidden relative'>
+            <ModelView />
+          </div>
+        </div>
       </div>
     </section>
   );
