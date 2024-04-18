1647de8f04f8f3c8a2ec1829bf4f41d74f47cef5
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-18 12:23:27 -0400

    add lights + camera

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,3 +1,16 @@
-export const ModelView = () => {
-  return <div>ModelView</div>;
+import { OrbitControls, PerspectiveCamera, View } from '@react-three/drei';
+
+import { Lights } from '../components';
+
+export const ModelView = ({ index, groupRef, gsapType, controlRef, setRotation, item, size }) => {
+  return (
+    <View id={gsapType} index={index} className={`w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}>
+      {/* camera */}
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      {/* lights */}
+      <ambientLight intensity={0.3} />
+      <Lights />
+    </View>
+  );
 };
