428394679b6a0a970cb21803ab4981c9f8e87a46
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-19 21:59:31 -0400

    add 'links'

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,26 @@ export const Footer = () => {
       <div className='screen-max-width'>
         <div>
           <p className='font-semibold text-gray text-xs'>
-            More ways to shop: <a className='underline text-blue' href='https://apple.com' target='_blank'>Find an Apple Store </a>
+            More ways to shop:{' '}
+            <a className='underline text-blue' href='https://apple.com' target='_blank'>
+              Find an Apple Store{' '}
+            </a>
             or <span className='underline text-blue'>other retailer</span> near you.
           </p>
           <p className='font-semibold text-gray text-xs'>Or call 000800-040-1966</p>
+        </div>
+
+        <div className='bg-neutral-700 my-5 h-[1px] w-full' />
+
+        <div className='flex md:flex-row flex-col md:items-center justify-between'>
+          <p className='font-semibold text-gray text-xs'>Copright @ 2024 Apple Inc. All rights reserved.</p>
+          <div className='flex'>
+            {footerLinks.map((link, i) => (
+              <p key={link} className='font-semibold text-gray text-xs'>
+                {link} {i !== footerLinks.length - 1 && <span className='mx-2'> | </span>}
+              </p>
+            ))}
+          </div>
         </div>
       </div>
     </footer>
