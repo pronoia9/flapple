1ec7b642d1f5b306aa8140aebd254203eeff5ef7
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-19 21:59:01 -0400

    add first section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,17 @@
+import { footerLinks } from '../data';
+
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='py-5 sm:px-10 px-5'>
+      <div className='screen-max-width'>
+        <div>
+          <p className='font-semibold text-gray text-xs'>
+            More ways to shop: <a className='underline text-blue' href='https://apple.com' target='_blank'>Find an Apple Store </a>
+            or <span className='underline text-blue'>other retailer</span> near you.
+          </p>
+          <p className='font-semibold text-gray text-xs'>Or call 000800-040-1966</p>
+        </div>
+      </div>
+    </footer>
+  );
 };
