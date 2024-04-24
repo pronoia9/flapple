ea54fd276fe219d85e409dfa00cf5a6298b7ad61
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-04-24 19:51:39 -0400

    add overflow-hidden

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import * as Sentry from '@sentry/react';
 import { Experience, Features, Footer, Hero, Highlights, HowItWorks, Navbar } from './components';
 
 export default Sentry.withProfiler(() => (
-  <main className='bg-black'>
+  <main className='bg-black overflow-hidden'>
     <Navbar />
     <Hero />
     <Highlights />
